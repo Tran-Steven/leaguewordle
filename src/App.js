1cc2aabf4c1c73f122164e36838ec3ca00a01c73
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-04-25 15:06:15 -0400

    League Of Legends X Wordle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+import logo from './league-of-wordle.png';
+import './App.css';
+import App2 from './App2';
+import { Route, NavLink, Switch } from 'react-router-dom'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+  function App() { 
+    return (
     <div className="App">
+
+<div className = "nav">
+<NavLink exact to ="/React" > App2</NavLink>
+</div>
+<Route render ={({location}) => (
+  <TransitionGroup>
+  <CSSTransition
+  key={location.key}
+  timeout={450}
+  classNames="fade"
+  >
+    <Switch>
+  <Route exact path="/App2">
+    <App2 />
+  </Route>
+  </Switch>
+  </CSSTransition>
+  </TransitionGroup>
+)}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="www.google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          test
         </a>
       </header>
+
+
+
     </div>
   );
 }
+
 
 export default App;
---
+++ src/App2.js
@@ -0,0 +1,18 @@
+import logo from './league-of-wordle.png';
+import './App.css';
+import { Route, NavLink, Switch } from 'react-router-dom'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+  function App2() {
+return (
+    <div className ="page">
+        <div className ="Wrapper">
+            <p> Sucessful test</p>
+        </div>
+    </div>
+)
+
+  }
+
+
+export default App2;
