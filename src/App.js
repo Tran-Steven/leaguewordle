90135f02f1bd6e2933b37e0aaee8e61b702b1e4b
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-04-29 20:45:30 -0400

    enter

--- src/App.js
+++ src/App.js
@@ -5,28 +5,13 @@ import App2 from './App2';
 import { Route, NavLink, Switch } from 'react-router-dom'
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
+
   function App() { 
     return (
+
     <div className="App">
 
-<div className = "nav">
-<NavLink exact to ="/React" > App2</NavLink>
-</div>
-<Route render ={({location}) => (
-  <TransitionGroup>
-  <CSSTransition
-  key={location.key}
-  timeout={450}
-  classNames="fade"
-  >
-    <Switch>
-  <Route exact path="/App2">
-    <App2 />
-  </Route>
-  </Switch>
-  </CSSTransition>
-  </TransitionGroup>
-)}/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -38,7 +23,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
           target="_blank"
           rel="noopener noreferrer"
         >
-          test
+
         </a>
       </header>
 
--- src/index.js
+++ src/index.js
@@ -6,15 +6,7 @@ import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
-  
-  ,document.getElementById('root')
-  
-  )
-  
+
   
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
@@ -23,7 +15,4 @@ ReactDOM.render(
     </React.StrictMode>
   );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
