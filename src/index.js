8df81cf1de4a5e19260591c7097e46d9114f4cce
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-07-17 19:39:03 -0400

    Fixed margin errors where the title image would not center in the middle by including it in a div. Made website render a new root after the title image dispeared.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import titleImage from './assets/league-of-wordle.png'
+import App from './App';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
+// import Home component & import About component
+import Home from "./components/Home";
+import About from "./components/About";
+  
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <img id="title" src={titleImage} alt="Title Screen"></img>
+ <div class = "image-container">
+  <img id ="title" src={titleImage} alt="Title Screen" class = "center"></img>
+ </div>
 )
+const home = ReactDOM.createRoot(document.getElementById('root'))
+
+
+setTimeout(function(){
+  home.render(
+    <div class = "main-load-in">
+    <App App="Main Page" />
+    </div>
+    );
+}, 5000);
