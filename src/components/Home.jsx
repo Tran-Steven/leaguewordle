ef319f9ab36b97ea2131a1f2e7600aa1c531766e
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-07-17 19:39:53 -0400

    Added the main page(home.jsx) and soon to be about page(about.jsx)

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+
+import React from "react";
+// importing Link from react-router-dom to navigate to 
+// different end points.
+import { Link } from "react-router-dom";
+  
+const Home = () => {
+  return (
+    <div>
+      <h1>League of Wordle</h1>
+      <br />
+      <ul>
+        <li>
+          {/* Endpoint to route to Home component */}
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          {/* Endpoint to route to About component */}
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          {/* Endpoint to route to Contact Us component */}
+          <Link to="/contactus">Contact Us</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+  
+export default Home;
