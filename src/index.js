f1b3e6c6d33d3e69ec4818cfb709cabed5c7c7f3
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-07-18 01:40:19 -0400

    Fixed formatting

--- src/index.js
+++ src/index.js
@@ -13,20 +13,20 @@ import {
 // import Home component & import About component
 import Home from "./components/Home";
 import About from "./components/About";
-  
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
- <div class = "image-container">
-  <img id ="title" src={titleImage} alt="Title Screen" class = "center"></img>
- </div>
+  <div class="image-container">
+    <img id="title" src={titleImage} alt="Title Screen" class="center"></img>
+  </div>
 )
 const home = ReactDOM.createRoot(document.getElementById('root'))
 
 
-setTimeout(function(){
+setTimeout(function () {
   home.render(
-    <div class = "main-load-in">
-    <App App="Main Page" />
+    <div class="main-load-in">
+      <App App="Main Page" />
     </div>
-    );
+  );
 }, 5000);
