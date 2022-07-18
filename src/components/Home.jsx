d4baf8e49fe61e331f25790c33f947559ac1ccf9
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-07-17 20:49:56 -0400

    Removed un needed jsx files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,18 +10,12 @@ const Home = () => {
       <h1>League of Wordle</h1>
       <br />
       <ul>
-        <li>
-          {/* Endpoint to route to Home component */}
-          <Link to="/">Home</Link>
-        </li>
+
         <li>
           {/* Endpoint to route to About component */}
           <Link to="/about">About</Link>
         </li>
-        <li>
-          {/* Endpoint to route to Contact Us component */}
-          <Link to="/contactus">Contact Us</Link>
-        </li>
+
       </ul>
     </div>
   );
