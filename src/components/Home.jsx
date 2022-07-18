5544e429aeefc78bef98ca9dc565f1671c0910f2
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-07-18 01:01:12 -0400

    Created the form button and input

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,35 @@
-
+import './Home.css'
 import React from "react";
 // importing Link from react-router-dom to navigate to 
 // different end points.
 import { Link } from "react-router-dom";
-  
+
 const Home = () => {
   return (
-    <div>
-      <h1>League of Wordle</h1>
-      <br />
-      <ul>
-
-        <li>
-          {/* Endpoint to route to About component */}
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+    <div class="main">
+      <div class = "images">
+          <img src={ require('./assets/league-of-wordle.png') } alt="LoLxWordle Icon" width ="312" height="119.6"/>
+      </div>
+      <div class="game">
+<h1>Welcome to League of Wordle!</h1>
+<form action = "Home.jsx" method ="GET">
+  <input type="text" name="txtbox" id="txtbox"/>
+<input id="guess_submit" class="guess_input" type="submit" value="Submit"/>
+</form>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+          <div class ="sub-image">
+          <img src={ require('./assets/github.png') } alt="Github Icon" width ="312" height="119.6"/>
+          <img src={ require('./assets/linkedin.png') } alt="Linkedin Icon" width ="312" height="119.6"/>
+          <img src={ require('./assets/mail.png') } alt="Mail Icon" width ="312" height="119.6"/>
+          <img src={ require('./assets/share.png') } alt="Share Icon" width ="312" height="119.6"/>
+        </div>
+          </div>
     </div>
   );
 };
