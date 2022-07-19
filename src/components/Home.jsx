18909ff5f1c1343e3d9d75332fc7b152d6a4b03a
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-07-19 01:49:22 -0400

    Fixed the form to store the input

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import './Home.css'
-import React from "react";
-// importing Link from react-router-dom to navigate to 
-// different end points.
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Home = () => {
+  const { register, handleSubmit } = useForm();
   return (
     <div class="main">
       <div class="images">
@@ -12,27 +12,27 @@ const Home = () => {
       </div>
       <div class="game">
         <h1>Welcome to League of Wordle!</h1>
-        <form action="Home.jsx" method="GET">
-          <input type="text" name="txtbox" id="txtbox" />
-          <input id="guess_submit" class="guess_input" type="submit" value="Submit" />
+
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
+          <input  {...register("guess")} class="guess_input" placeholder="Enter Champion Name Here" type="text" />
+          <input class="guess_input" type="submit" />
         </form>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <div class="sub-image">
-        <a href="https://github.com/Tran-Steven" target="_blank">  
-          <img src={require('./assets/github.png')} alt="Github Icon" width="312" height="119.6" />
+
+        <div class="sub-image" id="footer">
+          <a href="https://github.com/Tran-Steven" target="_blank" rel="noreferrer">
+            <img src={require('./assets/github.png')} alt="Github Icon" width="312" height="119.6" />
           </a>
-          <a href="https://www.linkedin.com/in/steven-tran-26735b206/" target="_blank">  
-          <img src={require('./assets/linkedin.png')} alt="Linkedin Icon" width="312" height="119.6" />
+          <a href="https://www.linkedin.com/in/steven-tran-26735b206/" target="_blank" rel="noreferrer">
+            <img src={require('./assets/linkedin.png')} alt="Linkedin Icon" width="312" height="119.6" />
           </a>
           <img src={require('./assets/mail.png')} alt="Mail Icon" width="312" height="119.6" />
           <img src={require('./assets/share.png')} alt="Share Icon" width="312" height="119.6" />
         </div>
+
       </div>
     </div>
   );
