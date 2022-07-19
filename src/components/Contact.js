bc795f062b92bfbd5b34a70eb596eb23d235cf26
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-07-19 13:18:18 -0400

    Added a way to send bugreports from the popup

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact(props) {
+    return (
+        <form method="post" action="mailto:visualsteven@gmail.com" enctype="text/plain" onSubmit="">
+                <div className="contactInfo">
+
+                    <textarea class ="input100" name="Bug/Issue"  ></textarea>
+                    <br/>
+                    <input type="submit" value="Send" /> 
+                </div>
+        </form>
+    );
+}
+export default Contact;
