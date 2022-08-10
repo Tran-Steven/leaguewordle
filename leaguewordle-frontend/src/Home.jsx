71234787c996bd027fde322719b11da3e49f45b6
Tran-Steven <64705534+Tran-Steven@users.noreply.github.com>
2022-08-10 11:30:57 -0400

    Worked removed function isDisabled to avoid deriving state into state

---
+++ leaguewordle-frontend/src/Home.jsx
@@ -0,0 +1,298 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card } from "antd";
+import { useForm } from "react-hook-form";
+import champions from "./data/champions.json";
+
+//css
+import "assets/css/Home.css";
+import "antd/lib/card/style/css";
+
+//imported .js files
+import { Wrong } from "./components/Wrong.js";
+import Popup from "./components/Popup";
+import Random from "./utils/Random";
+
+//imported images
+import wrong from "./assets/images/wrong.png";
+import down from "./assets/images/down.png";
+import higher from "./assets/images/higher.png";
+import correct from "./assets/images/checkmark.webp";
+
+const Home = () => {
+  // const initialState = {
+  //   buttonPopup: true,
+  //   isRight: true,
+  //   isWrong: true,
+  //   val: "",
+  //   sent: false,
+  //   text: "",
+  //   counter: 5,
+  //   champions1: champions,
+  //   championMatch: [],
+  // };
+  // const [state, dispatch] = useReducer(reducer, initialState);
+
+  //handles input submit
+  const { register, handleSubmit } = useForm();
+
+  const [buttonConPopup, setButtonConPopup] = useState(true);
+  const [isToggled, setIsToggled] = useState(false);
+
+  const [val, setVal] = useState("");
+  //State the contact popup is at
+  const [buttonPopup, setButtonPopup] = useState(true);
+
+  //If user is correct or wrong on their guess
+  const [isRight, setIsRight] = useState(false);
+  const [isWrong, setIsWrong] = useState(false);
+
+  // deals with the data from the form and sends an email
+  const [sent, setSent] = useState(false);
+  const [text, setText] = useState("");
+  //Sets the amount of guesses
+  const [counter, setCounter] = useState(5);
+
+  //List of champions for auto complete
+  const [champions1, setChampions1] = useState(champions);
+  const [championMatch, setChampionMatch] = useState([]);
+  const [setCorrectChampion, CorrectChampion] = useState(1);
+
+  const handleSend = async () => {
+    setSent(true);
+    try {
+      await axios.post("http://localhost:2525/send_mail", {
+        text,
+      });
+    } catch (error) {
+      console.log(error);
+      console.log("react error");
+    }
+  };
+  let championList = [];
+  for (let i = 0; i < champions.length; i++) {
+    championList[i] = champions[i].Champion.toUpperCase().replaceAll(" ", "");
+  }
+
+  const isLetters = (str) => /^[A-Za-z'&" "]*$/.test(str);
+
+  const searchChampions = (text) => {
+    if (!text) {
+      setChampionMatch([]);
+    } else {
+      let matches = champions1.filter((champion2) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return champion2.Champion.match(regex);
+      });
+      setChampionMatch(matches);
+    }
+  };
+
+  const isDisabled = () => {
+    let num = 1;
+    if (num === 0) {
+      return true;
+    } else if (num === 1) {
+      return false;
+    }
+  };
+
+  const randNumGen = function () {
+    var maxLimit = 161;
+    let rand = Math.random() * maxLimit;
+    rand = Math.floor(rand);
+    return rand;
+  };
+
+  // useEffect(() => {
+  //   const randNumGen = function () {
+  //     var maxLimit = 161;
+  //     let rand = Math.random() * maxLimit;
+  //     rand = Math.floor(rand);
+  //     return rand;
+  //     const correctChampion = champions[randNumGen()].Champion.valueOf()
+  //     .toString()
+  //     .toUpperCase()
+  //     .replaceAll(" ", "");
+  //   };
+  // }, []);
+  const correctChampion = champions[randNumGen()].Champion.valueOf()
+    .toString()
+    .toUpperCase()
+    .replaceAll(" ", "");
+  return (
+    <div className="main">
+      <header className="mainImage">
+        <img
+          src={require("./assets/league-of-wordle.png")}
+          alt="LoLxWordle Icon"
+          width="22%"
+        />
+        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          <img
+            src={require("./assets/league-of-wordle.png")}
+            alt="LoLxWordle Icon"
+            width="50%"
+          />
+          <h2 className="how-to-h">HOW TO PLAY</h2>
+          <p className="how-to">
+            Guess the League of legends champion within 5 tries. For every
+            unsuccessful try, a hint will be given showing if your BE Amount, RP
+            Amount, Release Date Year, or Champion Class is correct.
+            <br />
+            <br />
+            Each guess has to be a valid champion. Click the send button below
+            to submit.
+          </p>
+          <h2 className="found-bug">FOUND A BUG OR HAVE AN ISSUE?</h2>
+          <div className="formHolder">
+            {!sent ? (
+              <form
+                target="_top"
+                method="post"
+                action="mailto:visualsteven@gmail.com"
+                encType="text/plain"
+                onSubmit={handleSend}
+              >
+                <div className="contactInfo">
+                  <input
+                    autoComplete="off"
+                    className="input100"
+                    name="Bug/Issue"
+                    type="text"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                  />
+
+                  <br />
+                  <input type="submit" value="Send" />
+                </div>
+              </form>
+            ) : (
+              <h2 className="confirm">Email Sent Successfully!</h2>
+            )}
+          </div>
+        </Popup>
+      </header>
+
+      <main className="gameSection">
+        <h1 className="opener">Welcome to League of Wordle!</h1>
+        <p>Tries Available: {counter}</p>
+        <form
+          onSubmit={handleSubmit((val) => {
+            let userInput = val.guess;
+            userInput = userInput.toUpperCase().replaceAll(" ", "");
+            console.log("User's input: " + userInput);
+            console.log("Correct champion: " + correctChampion);
+            let champ3 = "";
+            let temp = champions1.filter((item) => {
+              const regex2 = new RegExp(`${userInput}`, "gi");
+              champ3 = item.Champion.match(regex2);
+            });
+            // console.log(champ3);
+            if (userInput !== correctChampion) {
+              // if(userInput.valueOf().toUpperCase())
+              // setIsWrong(true);
+              // setIsRight(false);
+              // if (counter == 0) {
+              //   num = 0;
+              // } else {
+              // setCounter(counter - 1);
+              // }
+            } else if (
+              userInput.valueOf().toUpperCase() ===
+              correctChampion.valueOf().toString().toUpperCase()
+            ) {
+              setIsWrong(false);
+              setIsRight(true);
+            }
+          })}
+        >
+          <input
+            {...register("guess")}
+            autoComplete="off"
+            className="guess_input"
+            placeholder="Enter Champion Name Here"
+            type="text"
+            value={val}
+            onChange={(e) => {
+              const { value } = e.target;
+              if (isLetters(value)) {
+                console.log(correctChampion);
+                setVal(value);
+                searchChampions(value);
+              }
+            }}
+          />
+          <input
+            type="submit"
+            isDisabled={championList.some(
+              (x) => x === val.toUpperCase().replaceAll(" ", "")
+            )}
+          />
+          {championMatch &&
+            championMatch.map((item, index) => (
+              <div key={index} style={{ marginLeft: "35%", marginTop: "5px" }}>
+                <Card style={{ width: "50%" }}>{item.Champion}</Card>
+              </div>
+            ))}
+          {isWrong && <Wrong text="Class" alt="wrong img" img={wrong} />}
+          {isRight && <Wrong text="Class" alt="correct img" img={correct} />}
+        </form>
+      </main>
+
+      <footer className="subImages">
+        <a
+          href="https://github.com/Tran-Steven"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={require("./assets/github.png")}
+            alt="Github Icon"
+            width="18%"
+            height="auto"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/steven-tran-26735b206/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={require("./assets/linkedin.png")}
+            alt="Linkedin Icon"
+            width="18%"
+            height="auto"
+          />
+        </a>
+        <img
+          src={require("./assets/mail.png")}
+          alt="Mail Icon"
+          width="18%"
+          height="auto"
+        />
+        <img
+          src={require("./assets/help.png")}
+          alt="Help Icon"
+          width="18%"
+          height="auto"
+          onClick={() => {
+            setButtonPopup(true);
+            setSent(false);
+            setText("");
+          }}
+          id="help"
+        />
+        <img
+          src={require("./assets/share.png")}
+          alt="Share Icon"
+          width="18%"
+          height="auto"
+        />
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
